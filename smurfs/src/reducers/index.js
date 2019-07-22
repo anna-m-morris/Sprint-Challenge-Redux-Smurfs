3cf394767175a435130acc5d9142ec5246347112
clay-most <46730958+clay-most@users.noreply.github.com>
2019-07-21 18:06:45 -0600

    build reducer and initial state

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,64 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAIIL
+} from '../actions/index';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+import {
+  GET_SMURF_START,
+  GET_SMURF_SUCCESS,
+  GET_SMURF_FAIIL
+} from '../actions/index';
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+import { ERROR } from '../actions/index';
+
+const initalState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  // updatingSmurf: false,
+  // deletingSmurf: false,
+  error: null
+};
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SMURF_START:
+      return {
+        ...state,
+        addingSmurf: true
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        addingSmurf: false
+      };
+    case ADD_SMURF_FAIL:
+      return {
+        ...state,
+        addingSmurf: false,
+        error: ERROR.paylaod
+      };
+    case GET_SMURF_START:
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    case GET_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false
+      };
+    case GET_SMURF_FAIL:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: ERROR.payload
+      };
+    default:
+      return { ...state };
+  }
+};
+export default smurfReducer;
